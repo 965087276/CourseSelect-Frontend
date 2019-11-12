0773b5417340f33c8a2d42d74a235f0c92e1df54
wangjinhao <965087276@qq.com>
2019-11-12 19:50:33 +0800

    项目初始化

---
+++ src/Common.vue
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    const host = "http://localhost:9000/xk/service01/api/";
+
+    const login_location = "/login.html";
+    const index_location = "/index.html";
+
+    export default {
+        login_location,
+        index_location,
+        host
+    };
+</script>
