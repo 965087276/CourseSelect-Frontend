1bac1f158cc4274f23fb8c76bda6c912d69ba3d5
wangjinhao <965087276@qq.com>
2019-11-26 11:09:44 +0800

    将课程筛选表单封装到课程列表组件

--- src/components/student/wCourseListTable.vue
+++ src/components/student/wCourseListTable.vue
@@ -1,81 +1,114 @@
 <template>
-    <el-table
-            :data="courseList"
-            :span-method="objectSpanMethod"
-            border
-            stripe
-            style="width: 100%">
-        <el-table-column
-                prop="college"
-                label="开课学院">
-        </el-table-column>
-        <el-table-column
-                prop="courseCode"
-                label="课程编码">
-        </el-table-column>
-        <el-table-column
-                prop="courseName"
-                label="课程名称">
-        </el-table-column>
-        <el-table-column
-                prop="courseType"
-                label="课程属性">
-        </el-table-column>
-        <el-table-column
-                prop="major"
-                label="所属学科/专业">
-        </el-table-column>
-        <el-table-column
-                prop="credit"
-                label="课时/学分">
-        </el-table-column>
-        <el-table-column
-                prop="limitNum"
-                label="限选">
-        </el-table-column>
-        <el-table-column
-                prop="selectedNum"
-                label="已选">
-        </el-table-column>
-        <el-table-column
-                prop="weekPeriod"
-                label="开课周">
-        </el-table-column>
-        <el-table-column
-                prop="sectionPeriod"
-                label="节次">
-        </el-table-column>
-        <el-table-column
-                prop="classroom"
-                label="教室">
-        </el-table-column>
-        <!-- <el-table-column
-            prop="teachingType"
-            label="授课方式">
-        </el-table-column> -->
-        <el-table-column
-                prop="examType"
-                label="考核方式">
-        </el-table-column>
-        <el-table-column
-                prop="courseTeacher"
-                label="主讲教师">
-        </el-table-column>
-        <el-table-column label="操作">
-            <template slot-scope="scope">
-                <!--<el-button-->
-                        <!--type="primary"-->
-                        <!--size="small"-->
-                        <!--@click="addPreCourse(scope.$index, scope.row)">预选</el-button>-->
-                <el-button
-                    type="primary"
-                    size="small"
-                    @click="$emit('add-pre-course', scope.$index, scope.row)">
-                    预选
-                </el-button>
-            </template>
-        </el-table-column>
-    </el-table>
+    <div id="app">
+        <el-form
+                :inline="true"
+                :model="formInline"
+                label-width="auto"
+                class="demo-form-inline">
+            <el-form-item label="开课学院">
+                <el-select v-model="formInline.college" filterable placeholder="请选择">
+                    <el-option v-for="item in colleges" :key="item.value" :label="item.label" :value="item.value"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="上课时间">
+                <el-select v-model="formInline.courseTime" filterable placeholder="请选择">
+                    <el-option v-for="item in colleges" :key="item.value" :label="item.label" :value="item.value"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="课程属性">
+                <el-select v-model="formInline.courseType" filterable placeholder="请选择">
+                    <el-option v-for="item in courseTypes" :key="item.value" :label="item.label" :value="item.value"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item label="课程名称">
+                <el-input v-model="formInline.courseName" placeholder="请输入"></el-input>
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" icon="el-icon-search"  @click="onSubmit">查询</el-button>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" icon="el-icon-download"  @click="exportData">导出</el-button>
+            </el-form-item>
+        </el-form>
+        <el-table
+                :data="courseList"
+                :span-method="objectSpanMethod"
+                border
+                stripe
+                style="width: 100%">
+            <el-table-column
+                    prop="college"
+                    label="开课学院">
+            </el-table-column>
+            <el-table-column
+                    prop="courseCode"
+                    label="课程编码">
+            </el-table-column>
+            <el-table-column
+                    prop="courseName"
+                    label="课程名称">
+            </el-table-column>
+            <el-table-column
+                    prop="courseType"
+                    label="课程属性">
+            </el-table-column>
+            <el-table-column
+                    prop="major"
+                    label="所属学科/专业">
+            </el-table-column>
+            <el-table-column
+                    prop="credit"
+                    label="课时/学分">
+            </el-table-column>
+            <el-table-column
+                    prop="limitNum"
+                    label="限选">
+            </el-table-column>
+            <el-table-column
+                    prop="selectedNum"
+                    label="已选">
+            </el-table-column>
+            <el-table-column
+                    prop="weekPeriod"
+                    label="开课周">
+            </el-table-column>
+            <el-table-column
+                    prop="sectionPeriod"
+                    label="节次">
+            </el-table-column>
+            <el-table-column
+                    prop="classroom"
+                    label="教室">
+            </el-table-column>
+            <!-- <el-table-column
+                prop="teachingType"
+                label="授课方式">
+            </el-table-column> -->
+            <el-table-column
+                    prop="examType"
+                    label="考核方式">
+            </el-table-column>
+            <el-table-column
+                    prop="courseTeacher"
+                    label="主讲教师">
+            </el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <!--<el-button-->
+                    <!--type="primary"-->
+                    <!--size="small"-->
+                    <!--@click="addPreCourse(scope.$index, scope.row)">预选</el-button>-->
+                    <el-button
+                            type="primary"
+                            size="small"
+                            @click="$emit('add-course', scope.$index, scope.row)">
+                        <slot name="course-select-text"></slot>
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <script>
@@ -88,15 +121,47 @@
                 type: Array,
                 default: []
             },
-            // // spanArray
-            // spanArr: {
-            //     type: Array,
-            //     default: []
-            // }
+            // formInline
+            formInline: {
+                type: Object,
+                default: []
+            }
         },
         data() {
             return {
-                spanArr: []
+                spanArr: [],
+                colleges: [{
+                    value: '选项1',
+                    label: '黄金糕'
+                }, {
+                    value: '选项2',
+                    label: '双皮奶'
+                }, {
+                    value: '选项3',
+                    label: '蚵仔煎'
+                }, {
+                    value: '选项4',
+                    label: '龙须面'
+                }, {
+                    value: '选项5',
+                    label: '北京烤鸭'
+                }],
+                courseTypes: [{
+                    value: '选项1',
+                    label: '核心课'
+                }, {
+                    value: '选项2',
+                    label: '普及课'
+                }, {
+                    value: '选项3',
+                    label: '研讨课'
+                }, {
+                    value: '选项4',
+                    label: '公共选修课'
+                }, {
+                    value: '选项5',
+                    label: '公共必修课'
+                }],
             }
         },
         methods: {
--- src/views/student/CourseList.vue
+++ src/views/student/CourseList.vue
@@ -1,41 +1,14 @@
 <template>
     <el-container>
-        <!-- <el-header>
-            
-        </el-header> -->
         <el-main>
-            <el-form 
-                :inline="true" 
-                :model="formInline" 
-                label-width="auto"
-                class="demo-form-inline">
-                <el-form-item label="开课学院">
-                    <el-select v-model="formInline.college" filterable placeholder="请选择">
-                        <el-option v-for="item in colleges" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="上课时间">
-                    <el-select v-model="formInline.courseTime" filterable placeholder="请选择">
-                        <el-option v-for="item in colleges" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="课程属性">
-                    <el-select v-model="formInline.courseType" filterable placeholder="请选择">
-                        <el-option v-for="item in courseTypes" :key="item.value" :label="item.label" :value="item.value"></el-option>
-                    </el-select>
-                </el-form-item>
-                <el-form-item label="课程名称">
-                    <el-input v-model="formInline.courseName" placeholder="请输入"></el-input>
-                </el-form-item>
-                
-                <el-form-item>
-                    <el-button type="primary" icon="el-icon-search"  @click="onSubmit">查询</el-button>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" icon="el-icon-download"  @click="exportData">导出</el-button>
-                </el-form-item>
-            </el-form>
-            <w-course-list :courseList="courseList" v-on:add-pre-course="addPreCourse"></w-course-list>
+            <w-course-list
+                    v-model="formInline"
+                    :courseList="courseList"
+                    v-on:add-course="addPreCourse">
+                <template v-slot:course-select-text>
+                    预选
+                </template>
+            </w-course-list>
             <el-pagination
                 background
                 layout="prev, pager, next"
@@ -61,38 +34,6 @@
                 totalElements: 100,
                 curPage: 1,
                 pageSize: 20,
-                colleges: [{
-                    value: '选项1',
-                    label: '黄金糕'
-                }, {
-                    value: '选项2',
-                    label: '双皮奶'
-                }, {
-                    value: '选项3',
-                    label: '蚵仔煎'
-                }, {
-                    value: '选项4',
-                    label: '龙须面'
-                }, {
-                    value: '选项5',
-                    label: '北京烤鸭'
-                }],
-                courseTypes: [{
-                    value: '选项1',
-                    label: '核心课'
-                }, {
-                    value: '选项2',
-                    label: '普及课'
-                }, {
-                    value: '选项3',
-                    label: '研讨课'
-                }, {
-                    value: '选项4',
-                    label: '公共选修课'
-                }, {
-                    value: '选项5',
-                    label: '公共必修课'
-                }],
                 formInline: {
                     courseCode: '',
                     courseName: '',
