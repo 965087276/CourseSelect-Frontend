6443f0b10c52463efe229194b7d635e982d0d466
wangjinhao <965087276@qq.com>
2019-11-27 00:55:41 +0800

    添加我的课表功能

---
+++ src/views/student/MyCourse.vue
@@ -0,0 +1,190 @@
+<template>
+    <div id="studentCourse">
+        <h1>学生课表</h1>
+        <div class="table">
+            <div class="table_head">
+                <div class="day">周一</div>
+                <div class="day">周二</div>
+                <div class="day">周三</div>
+                <div class="day">周四</div>
+                <div class="day">周五</div>
+                <div class="day">周六</div>
+                <div class="day">周日</div>
+            </div>
+            <div class="table_main">
+                <div class="table_aside">
+                    <div class="time">1</div>
+                    <div class="time">2</div>
+                    <div class="time">3</div>
+                    <div class="time">4</div>
+                    <div class="time">5</div>
+                </div>
+                <div class="table_content">
+                    <div class="class" v-for="(item,index) in data" :key="index">
+                        <div class="class_inner" v-if="item.courseCode!=''">
+                            <div class="course">{{item.courseName}}</div>
+                            <div class="class_info">
+                                <div class="student">
+                                    <i class="el-icon-info"></i>
+                                    {{item.courseTeacher}}
+                                </div>
+                                <div class="classroom">
+                                    <i class="el-icon-location"></i>
+                                    {{item.classroom}}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="no_class" v-if="item.courseCode == ''"  @click="addCourse(index)">无课</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import * as studentAPI from '@/api/student/api-student.js'
+    export default {
+        data() {
+            return {
+                data: []
+            };
+        },
+        methods: {
+            addCourse(index) {
+                alert(index)
+            },
+            parseData(data) {
+                let finalData = [];
+                for (let i = 0; i < 35; i++) {
+                    finalData[i] = {
+                        courseCode: "",
+                        classroomId: "",
+                        day: "",
+                        time: "",
+                        classroom: "",
+                        courseName: ""
+                    };
+                }
+                for (let i in data) {
+                    let day = parseInt(data[i].day);
+                    let time = parseInt(data[i].time);
+                    let index = (day - 1) * 5 + time - 1;
+                    finalData[index] = data[i];
+                }
+                return finalData;
+            },
+            getStudentClass() {
+                studentAPI.getStudentCourse(this.$store.state.username)
+                    .then(res => {
+                        this.data = this.parseData(res.courses);
+                    });
+            }
+        },
+        mounted() {
+            this.getStudentClass();
+        }
+    };
+</script>
+
+<style lang='scss' scoped>
+    .table_head {
+        padding: 20px;
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+        margin-bottom: 20px;
+        display: flex;
+        margin-left: 85px;
+        .day {
+            width: 14.286%;
+            text-align: center;
+        }
+    }
+    .table_main {
+        display: flex;
+        .table_aside {
+            width: 50px;
+            height: 750px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+            padding: 10px;
+            margin-right: 20px;
+            .time {
+                line-height: 150px;
+                text-align: center;
+                font-size: 1.5rem;
+            }
+        }
+        .table_content {
+            height: 750px;
+            background-color: #fff;
+            border-radius: 10px;
+            padding: 10px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            flex-wrap: wrap;
+            width: 100%;
+            .class {
+                height: 150px;
+                width: 14.28%;
+                padding: 10px;
+                box-sizing: border-box;
+                @media screen and (max-width: 1540px) {
+                    font-size: 14px;
+                    .course {
+                        min-height: 50px !important;
+                        font-size: 1rem !important;
+                    }
+                    .class_info {
+                        display: block !important;
+                        line-height: 20px !important;
+                    }
+                }
+                .class_inner {
+                    background-color: #409eff;
+                    height: 100%;
+                    border-radius: 10px;
+                    padding: 20px;
+                    box-sizing: border-box;
+                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+                    .course {
+                        color: white;
+                        font-size: 1.2rem;
+                        font-weight: bold;
+                        min-height: 65px;
+                    }
+                    .class_info {
+                        display: flex;
+                        line-height: 30px;
+                        color: white;
+                        .student {
+                            margin-right: 10px;
+                        }
+                    }
+                }
+                .no_class {
+                    background-color: rgba(0, 0, 0, 0.05);
+                    height: 100%;
+                    border-radius: 10px;
+                    padding: 20px;
+                    box-sizing: border-box;
+                    line-height: 90px;
+                    text-align: center;
+                    font-size: 1.2rem;
+                    font-weight: lighter;
+                    color: rgba(0, 0, 0, 0.3);
+                    cursor: pointer
+                }
+            }
+        }
+    }
+    .notifys {
+        background-color: #409eff !important;
+        color: #fff !important;
+    }
+</style>
