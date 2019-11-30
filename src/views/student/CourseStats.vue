a1022696137e76aa5d67cd39a41c600eef4f300f
wangjinhao <965087276@qq.com>
2019-11-30 09:19:23 +0800

    初始化选课学分统计页面

---
+++ src/views/student/CourseStats.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-container>
+        <el-header>
+
+        </el-header>
+        <el-main>
+
+        </el-main>
+    </el-container>
+</template>
+
+
+<script>
+    export default {
+        data() {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
