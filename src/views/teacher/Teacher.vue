0ae358580638e3538db11aa4bcd3310e4789b8d1
klfshuai <965082013@qq.com>
2019-12-05 15:57:01 +0800

    增加教师主界面

--- src/views/teacher/Teacher.vue
+++ src/views/teacher/Teacher.vue
@@ -1,38 +1,63 @@
 <template>
-    <div id="login">
-        Teacher
+    <div id="admin" class="full_screen">
+        <el-container>
+            <el-aside>
+                <user-info class="user_info"></user-info>
+                <el-menu
+                    class="nav_bar"
+                    default-active="teacher"
+                    background-color="#1d2b36"
+                    text-color="#fff"
+                    active-text-color="#ffd04b"
+                    @select="selectMenu"
+                >
+                    <el-submenu index="course-manager">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>课程管理</span>
+                        </template>
+                        <el-menu-item index="course-maneger/add">增添课程</el-menu-item>
+                        <el-menu-item index="course-maneger/grades">录入成绩</el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="courseList">
+                    <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>查看上课课表</span>
+                    </template>
+                </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <transition>
+                    <router-view class="router"></router-view>
+                </transition>
+            </el-main>
+        </el-container>
     </div>
 </template>
 
 <script>
+    import userInfo from "@/components/user/UserInfo"
     export default {
         data() {
             return {}
         },
-        methods: {},
+        components: {
+            "user-info": userInfo
+        },
+        methods: {
+            selectMenu(index) {
+                this.$router.push("/teacher/" + index);
+            },
+            exit() {
+                
+            }
+            
+        },
         mounted() {}
     };
 </script>
 
 <style lang="scss" scoped>
-    #login {
-        .title {
-            font-size: 1.5rem;
-        }
-        .btn_box {
-            float: right;
-            @media screen and (max-width: 560px) {
-                float: initial;
-                display: flex;
-                justify-content: center;
-                margin-left: -50px;
-                margin-top: 10px;
-            }
-        }
-        .rememberPwd {
-            margin-left: 50px;
-            margin-bottom: 20px;
-        }
-    }
+    
 </style>
-
