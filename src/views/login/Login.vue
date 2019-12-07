2cf396ef52ecdeff835ab08006a0181d8bbc26cc
wangjinhao <965087276@qq.com>
2019-12-07 09:09:46 +0800

    修改登录界面为弹框

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,26 +1,25 @@
 <template>
     <div id="login">
-        <div class="title">用户登录</div>
-        <hr>
-        <el-form 
-            ref="loginForm" 
-            :rules="rules"
-            :model="loginForm" 
-            label-width="80px"
-        >
-            <el-form-item label="账号" prop="username">
-                <el-input v-model="loginForm.username"></el-input>
-            </el-form-item>
-            <el-form-item label="密码" prop="password">
-                <el-input type="password" v-model="loginForm.password"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="loginClick">登录</el-button>
-                <el-button type="success" @click="$router.push('/register')">注册</el-button>
-                <el-button type="success" @click="forgetPasswordClick">忘记密码</el-button>
-            </el-form-item>
-        </el-form>
-            
+        <el-dialog title="用户登录" :visible.sync="dialogTableVisible" width="40%">
+            <el-form
+                    ref="loginForm"
+                    :rules="rules"
+                    :model="loginForm"
+                    label-width="80px"
+            >
+                <el-form-item label="账号" prop="username">
+                    <el-input v-model="loginForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input type="password" v-model="loginForm.password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="loginClick">登录</el-button>
+                    <el-button type="success" @click="$router.push('/register')">注册</el-button>
+                    <el-button type="success" @click="forgetPasswordClick">忘记密码</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
     </div>
 </template>
 
@@ -30,6 +29,7 @@
     export default {
         data() {
             return {
+                dialogTableVisible: true,
                 loginForm: {
                     username: '',
                     password: ''
