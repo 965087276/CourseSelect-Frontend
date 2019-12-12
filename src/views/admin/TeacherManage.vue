8e20c0c6b3938217d79edafce38f76f54f1f8ed5
klfshuai <965082013@qq.com>
2019-12-12 20:45:34 +0800

    修改获取学院函数中arr.colleges为arr

--- src/views/admin/TeacherManage.vue
+++ src/views/admin/TeacherManage.vue
@@ -343,7 +343,7 @@
             getColleges() {
                 pubAPI.getColleges()
                     .then(arr => {
-                        arr.colleges.forEach(item => {
+                        arr.forEach(item => {
                             this.colleges.push({ label: item, value: item })
                         })
                     })
